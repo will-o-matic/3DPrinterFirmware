32f6ab13107f513e955cc1abd13598948072aaa5
WillOMatic <github@liska.me>
2023-03-10 11:09:11 -0500

    Renamed Defective-Joe to Goldsteel.

--- Goldsteel/Marlin-2.1.2/Marlin/Configuration.h
+++ Goldsteel/Marlin-2.1.2/Marlin/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Defective Joe - 4.2.2"
+#define CUSTOM_MACHINE_NAME "Goldsteel"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
