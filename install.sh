25fc31cca4eafa798b99011fe1527ccac28cb728
WillOMatic <github@liska.me>
2023-03-21 19:29:03 -0400

    Updated readme

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-echo "Uploading from pi to github..." 
+# make a backup of printer.cfg
+echo "Backing up printer.cfg to printer.backup..." 
+mv ~/printer.cfg ~/printer.backup
 
-echo "Moving files"
-cp -v  ~/printer.cfg ~/cr10_klipper/$PRINTER_NAME/printer.cfg
+# pull latest
+echo "Pulling..." 
+git pull https://github.com/fl0r1s/cr10_klipper/
 
-echo "Adding to git"
-cd ~/cr10_klipper
-git add $PRINTER_NAME/printer.cfg
-git commit -m "Uploaded calibration settings" 
+# copy printer.cfg for this printer
+echo "Copying the printer.cfg file for $PRINTER_NAME..." 
+cp -v -r ~/cr10_klipper/$PRINTER_NAME/printer.cfg ~/printer.cfg
 
-echo "Pushing..." 
-git push origin master
+# ask if user wants to restore the calib parameters
+read -p "Do you want to restore printer.cfg (calibration parameters)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp ~/printer.backup ~/printer.last_known_cfg
+    mv ~/printer.backup ~/printer.cfg
+fi
